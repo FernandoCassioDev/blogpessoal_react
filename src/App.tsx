84222481e5b790c8f3939cba572f8ca2236ee26f
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-23 19:43:15 -0300

    feat: adiciona rotas na app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[85vh]'>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
