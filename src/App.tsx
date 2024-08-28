f616b4d3cc537dfdb10b8fb207a8e3238edadec9
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-28 14:45:23 -0300

    feat: adiciona rota para listar tema

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listaTemas/ListaTemas'
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/temas" element={<ListaTemas />} />
         </Routes>
         </div>
         <Footer />
