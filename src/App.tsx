17d5240bf6f51c47cbf7093f169cace399051d25
Fernando Cassio <fernandocassiodev@gmail.com>
2024-09-03 13:14:44 -0300

    feat: importa react-toastify e adiciona rotas para cadastro, edição, deletar postagens e perfil de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,17 @@ import ListaTemas from "./components/temas/listatemas/ListaTemas";
 import FormTema from "./components/temas/formulariotema/FormTema";
 import DeletarTema from "./components/temas/deletartema/Deletar";
 import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
+import FormularioPostagem from "./components/postagem/formulariopostagem/FormularioPostagem";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import Perfil from "./pages/perfil/Perfil";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 
 function App() {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[85vh]">
@@ -28,6 +34,22 @@ function App() {
               <Route path="/editarTema/:id" element={<FormTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
               <Route path="/postagens" element={<ListaPostagens />} />
+              <Route
+                path="/cadastroPostagem"
+                element={<FormularioPostagem />}
+              />
+              <Route
+                path="/editarPostagem/:id"
+                element={<FormularioPostagem />}
+              />
+              <Route
+                path="/deletarPostagem/:id"
+                element={<DeletarPostagem />}
+              />
+              <Route
+                path="/perfil"
+                element={<Perfil />}
+              />
             </Routes>
           </div>
           <Footer />
